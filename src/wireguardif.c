4c8e9677388978bd629f22ffee38b454f91e3a35
tac-yacht <tac-yacht@users.noreply.github.com>
2025-04-12 18:42:03 +0900

    ESP-IDF version up(4.1) migration https://docs.espressif.com/projects/esp-idf/en/v4.1/api-reference/network/tcpip_adapter_migration.html

--- src/wireguardif.c
+++ src/wireguardif.c
@@ -48,7 +48,15 @@
 #include "wireguard.h"
 #include "crypto.h"
 #include "esp_log.h"
+
+#include "esp_idf_version.h"
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0)
+#define ESP_IDF
+#include "esp_netif.h"
+#include "esp_netif_net_stack.h"
+#else
 #include "tcpip_adapter.h"
+#endif
 
 #include "esp32-hal-log.h"
 
@@ -921,7 +929,12 @@ err_t wireguardif_init(struct netif *netif) {
 	size_t private_key_len = sizeof(private_key);
 
 	struct netif* underlying_netif;
+#ifdef ESP_IDF
+	underlying_netif=esp_netif_get_netif_impl(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"));
+	//key -> https://github.com/espressif/esp-idf/blob/v4.1/components/esp_netif/esp_netif_defaults.c#L42
+#else //tcpip_adapter.h 4.1>version
 	tcpip_adapter_get_netif(TCPIP_ADAPTER_IF_STA, &underlying_netif);
+#endif
 	log_i(TAG "underlying_netif = %p", underlying_netif);
 
 	LWIP_ASSERT("netif != NULL", (netif != NULL));
