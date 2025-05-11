03a54da85b6a6c8f9e73f49fa50fc4d585189aeb
tac-yacht <tac-yacht@users.noreply.github.com>
2025-05-11 15:07:09 +0900

    厳格にC言語指定

--- src/default_netif.h
+++ src/default_netif.h
@@ -1,8 +1,17 @@
 #ifndef _DEFAULT_NETIF_H_
 #define _DEFAULT_NETIF_H_
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "lwip/netif.h"
 
 struct netif* get_default_netif();
 
 #endif /* _DEFAULT_NETIF_H_ */
+
+#ifdef __cplusplus
+}
+#endif
